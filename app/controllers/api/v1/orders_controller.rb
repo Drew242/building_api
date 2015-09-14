f15471b578370b1ad74ffe43ec5054bac4c5ddca
Drew Conly <atconly@gmail.com>
2015-09-14 11:23:06 -0600

    add orders api

---
+++ app/controllers/api/v1/orders_controller.rb
@@ -0,0 +1,29 @@
+class Api::V1::OrdersController < ApplicationController
+    respond_to :json, :xml
+
+    def index
+      respond_with Order.all
+    end
+
+    def show
+      respond_with Order.find_by(id: params[:id])
+    end
+
+    def create
+      respond_with Order.create(order_params)
+    end
+
+    def update
+      respond_with Order.update(params[:id], order_params)
+    end
+
+    def destroy
+      respond_with Order.destroy(params[:id])
+    end
+
+    private
+
+    def order_params
+      params.require(:order).permit(:amount)
+    end
+end
